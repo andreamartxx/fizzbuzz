bae08df51d655323af66943e3adb19e1424619ba
Andrea Martin <amartin.onda@gmail.com>
2022-03-24 11:54:07 +0100

    Refactor logic to return the same number

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,10 +1,3 @@
 module.exports = function(number) {
-    if(number == 1){
-        return 1;
-    }else if(number == 2){
-        return 2;
-    }else if(number == 4){
-        return 4;
-    }
-    
+    return number;
 };
