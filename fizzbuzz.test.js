0c7f9477437c64ba32ae055c4879ed83cce5f95e
Andrea Martin <amartin.onda@gmail.com>
2022-03-24 12:07:31 +0100

    modify the tests from returning number to a string

--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -2,14 +2,14 @@ const fizzbuzz = require('./fizzbuzz');
 
 describe('fizzbuzz', () => {
     test('returns 1 for 1', () => {
-        expect(fizzbuzz(1)).toEqual(1);
+        expect(fizzbuzz(1)).toEqual('1');
     });
 
     test('returns 2 for 2', () => {
-        expect(fizzbuzz(2)).toEqual(2);
+        expect(fizzbuzz(2)).toEqual('2');
     });
 
     test('returns 4 for 4', () => {
-        expect(fizzbuzz(4)).toEqual(4);
+        expect(fizzbuzz(4)).toEqual('4');
     });
 });
