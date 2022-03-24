e2b89cc9f59980217778bd97a41a9267c023bdea
Andrea Martin <amartin.onda@gmail.com>
2022-03-24 11:28:49 +0100

    Add failing test for the case of 2

--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -4,4 +4,8 @@ describe('fizzbuzz', () => {
     test('returns 1 for 1', () => {
         expect(fizzbuzz(1)).toEqual(1);
     });
+
+    test('returns 2 for 2', () => {
+        expect(fizzbuzz(2)).toEqual(2);
+    });
 });
