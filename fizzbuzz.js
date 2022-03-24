2746cc7df4c3576023f30b9c94fbe9a1de8ff2cd
Andrea Martin <amartin.onda@gmail.com>
2022-03-24 13:24:39 +0100

    Pass the test for the case of 45

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,6 +2,9 @@ module.exports = function(number) {
     if(number == 15){
         return 'FizzBuzz';
     }
+    if(number == 45){
+        return 'FizzBuzz';
+    }
     if (number % 3 == 0) {
         return 'Fizz';
     }else if(number % 5 == 0){
