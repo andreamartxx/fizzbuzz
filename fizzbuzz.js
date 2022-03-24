176c23ade3d597c19072ba8bc999df630a7be14c
Andrea Martin <amartin.onda@gmail.com>
2022-03-24 12:31:00 +0100

    Refactor cases that are divible by 3

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,11 +1,5 @@
 module.exports = function(number) {
-    if (number == 3) {
-        return 'Fizz';
-    }
-    if (number == 6) {
-        return 'Fizz';
-    }
-    if (number == 9) {
+    if (number % 3 == 0) {
         return 'Fizz';
     }
     return number.toString();
