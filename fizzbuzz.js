ba95806b4c0054d420aebf7e6a4ae3d822240dfa
Andrea Martin <amartin.onda@gmail.com>
2022-03-25 11:24:41 +0100

    Refactor cases that are divisible by 3 and 5

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,12 +1,5 @@
 module.exports = function(number) {
-    if(number % 15 == 0){
-        return 'FizzBuzz';
-    }
-    if (number % 3 == 0) {
-        return 'Fizz';
-    }else if(number % 5 == 0){
-        return 'Buzz';
-    }
     
-    return number.toString();
+    return (`${number % 3 ? '' : 'Fizz'}${number % 5 ? '' : 'Buzz'}` || number.toString());
+
 };
