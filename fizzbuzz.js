b96e827b42760e41eef25b65de85557e52bf83bd
Andrea Martin <amartin.onda@gmail.com>
2022-03-25 13:44:28 +0100

    CLI for fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,21 @@
-module.exports = function(number) {
+function fizzbuzz(number) {
     
     return (`${number % 3 ? '' : 'Fizz'}${number % 5 ? '' : 'Buzz'}${number % 7 ? '':'Bang'}` || number.toString());
 
 };
+
+function print(){
+
+    const prompt = require("prompt-sync")();
+
+    num = prompt("Enter a number: ");
+
+    console.log(`${num % 3 ? '' : 'Fizz'}${num % 5 ? '' : 'Buzz'}${num % 7 ? '':'Bang'}` || num.toString());
+    
+}
+
+print();
+
+module.exports = fizzbuzz();
+
+
